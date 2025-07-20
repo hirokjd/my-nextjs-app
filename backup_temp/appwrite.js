1475b620e359f9299e105f64deae72e0a1f4e0c2
biarkr <bikusarkar54@gmail.com>
2025-07-20 14:27:54 +0000

    deleted unused

--- backup_temp/appwrite.js
+++
@@ -1,9 +0,0 @@
-import { Client, Account } from 'appwrite';
-
-const client = new Client();
-client.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT);
-client.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
-
-const account = new Account(client);
-
-export { account };
